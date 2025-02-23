929c2a3baac38a8af475aae3d4effbdb5b6c7ed9
Túlio Augusto <tulioalmeida67@gmail.com>
2025-02-23 18:08:11 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -1,9 +1,5 @@
-const usuario = { 
-  nome: "Raphael", 
-  matricula: "123456", 
-  pendencia: false, 
-  acessibilidade: true 
-};
+// objeto do usuário
+const usuario = { nome: "Raphael", matricula: "123456", pendencia: false, acessibilidade: true };
 
 // lista objetos de armários
 const armarios = [
@@ -23,7 +19,7 @@ function reservarArmario() {
   // obter tipo de armário selecionado pelo usuário no html.
   let tipoSelecionado = document.getElementById("tipoArmario").value;
   
-  // na lista, filtrar apenas os armários que estão disponíveis e que são acessíveis ao usuário.
+  // na lista, filtrar apenas os armários que estão disponíveis e que são acessiveis ao usuário.
   let armariosDisponiveis = armarios.filter(a => a.formato === tipoSelecionado && a.status === true && usuario.acessibilidade === a.acessivel);
   
   // caso não exista armário disponível, retorna para o usuário mensagem.
@@ -32,27 +28,19 @@ function reservarArmario() {
     return;
   }
   
-  // Caso exista armário(s) disponível, seguimos sorteando uma opção. 
+  // Caso exista armário(s) disponíveil, seguimos sorteando uma opção. 
   let armarioSorteado = armariosDisponiveis[Math.floor(Math.random() * armariosDisponiveis.length)];
   
-  // Depois localizamos o armário emprestado na lista de armários e mudamos o status do armário.
-  let armarioEmprestado = armarios.find(armario => armario.id === armarioSorteado.id);
-  armarioEmprestado.status = false;
+  // Depois localizamos o armário emprestado na lista de armarios e mudamos o status do armário.
+  let armarioEmprestado = armarios.find(armario => armario.id === armarioSorteado.id).status = false;
   
-  // Obter a data e hora do momento da reserva
-  let dataReserva = new Date();
-  armarioEmprestado.dataReserva = dataReserva;
-  
-  // Calcular a data e hora para entrega das chaves (prazo de 24h)
-  let dataEntrega = new Date(dataReserva.getTime() + 24 * 60 * 60 * 1000);
-  armarioEmprestado.dataEntrega = dataEntrega;
-  
-  // Finalmente, mudamos a pendência do usuário para verdadeira.
+  // Finalmente, mudamos a pendencia do usuário para verdadeira.
   usuario.pendencia = true;
   
-  // Imprimimos uma mensagem de reserva para o usuário.
-  document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! O armário ${armarioSorteado.id} foi reservado com sucesso! Você deve devolver as chaves até ${dataEntrega.toLocaleString()}.`;
+  // Impmimimos uma mensagem de reserva para o usuário.
+  document.getElementById("resultado").innerText = `Olá, ${usuario.nome}! O armário ${armarioSorteado.id} foi reservado com sucesso!`;
 
   console.log(usuario);
   console.log(armarios);
+
 }
